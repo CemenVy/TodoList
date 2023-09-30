6c23d91ee1f7ad8ee45076bf46d9d0faddb2b096
CemenVy <semen.vydrin0108@gmail.com>
2023-09-30 16:16:12 +0400

    Got the project ready to go

---
+++ TodoList/Models/TaskList.swift
@@ -0,0 +1,8 @@
+//
+//  TaskList.swift
+//  TodoList
+//
+//  Created by Семен Выдрин on 30.09.2023.
+//
+
+import Foundation
---
+++ TodoList/ViewControllers/TasksViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TasksViewController.swift
+//  TodoList
+//
+//  Created by Семен Выдрин on 30.09.2023.
+//
+
+import Foundation
