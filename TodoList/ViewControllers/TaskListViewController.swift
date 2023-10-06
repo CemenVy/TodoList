136d0864c5d47edc9e0feeef94bbf06401dd364f
CemenVy <semen.vydrin0108@gmail.com>
2023-10-06 14:12:54 +0400

    Work with logic and UI in TaskListViewController

--- TodoList/ViewControllers/TaskListViewController.swift
+++ TodoList/ViewControllers/TaskListViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 import RealmSwift
 
-final class TaskListViewController: UITableViewController {
+final class TaskListViewController: UIViewController {
+    
+    // MARK: - Private Properties
     private let cellID = "taskCell"
     
     private lazy var segmentedControl: UISegmentedControl = {
-        let segmentedControl = UISegmentedControl()
-        let items = ["Date", "A-z"]
+        let segmentedControl = UISegmentedControl(items: ["Date", "A-z"])
         segmentedControl.selectedSegmentIndex = 0
         segmentedControl.addTarget(
             self,
@@ -24,6 +25,10 @@ final class TaskListViewController: UITableViewController {
         return segmentedControl
     }()
     
+    private lazy var tableView = UITableView(frame: .zero, style: .plain)
+    
+    private lazy var stackView = UIStackView(arrangedSubviews: [segmentedControl, tableView])
+    
     private var taskLists: Results<TaskList>!
     private var storageManager = StorageManager.shared
     
@@ -32,30 +37,68 @@ final class TaskListViewController: UITableViewController {
         super.viewDidLoad()
         setupView()
         setupNavigationBar()
-        view.addSubview(segmentedControl)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        setupSubviews(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
         setConstraints()
         
-        taskLists = storageManager.fetchData(TaskList.self)
-        
+        taskLists = storageManager.realm.objects(TaskList.self)
+        createTempData()
     }
     
-    // MARK: - UITableViewDataSource
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tableView.isEditing = editing
+    }
+    
+    // MARK: - Private methods
+    @objc private func addNewTask() {
+        showAlert()
+    }
+    
+    @objc private func sortingList(_ sender: UISegmentedControl) {
+        taskLists = sender.selectedSegmentIndex == 0
+        ? taskLists.sorted(byKeyPath: "date")
+        : taskLists.sorted(byKeyPath: "title")
+        tableView.reloadData()
+    }
+    
+    private func createTempData() {
+        if !UserDefaults.standard.bool(forKey: "done") {
+            DataManager.shared.createTempData { [unowned self] in
+                UserDefaults.standard.set(true, forKey: "done")
+                tableView.reloadData()
+            }
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension TaskListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         taskLists.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        var content = cell.defaultContentConfiguration()
-        let taskLists = taskLists[indexPath.row]
-        content.text = taskLists.title
-        content.secondaryText = taskLists.tasks.count.formatted()
-        cell.contentConfiguration = content
+        let taskList = taskLists[indexPath.row]
+        cell.configure(with: taskList)
         return cell
     }
-    
-    // MARK: - UITableViewDelegate
-    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+}
+
+// MARK: - UITableViewDelegate
+extension TaskListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let taskList = taskLists[indexPath.row]
         
         let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [unowned self] _, _, _ in
@@ -82,33 +125,39 @@ final class TaskListViewController: UITableViewController {
         return UISwipeActionsConfiguration(actions: [deleteAction, editAction, doneAction])
     }
     
-   
-    // MARK: - Private methods
-    @objc private func addNewTask() {
-        showAlert()
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let taskList = taskLists[indexPath.row]
+            storageManager.delete(taskList)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
     
-    @objc private func sortingList(_ sender: UISegmentedControl) {
-        
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let taskList = taskLists[indexPath.row]
+        let tasksVC = TasksViewController()
+        tasksVC.taskList = taskList
+        navigationController?.pushViewController(tasksVC, animated: true)
     }
 }
 
 // MARK: - AlertController
-private extension TaskListViewController {
-    func showAlert(with taskList: TaskList? = nil, completion: (() -> Void)? = nil) {
+extension TaskListViewController {
+    private func showAlert(with taskList: TaskList? = nil, completion: (() -> Void)? = nil) {
         let alertBuilder = AlertControllerBuilder(
             title: taskList != nil ? "Edit List" : "New List",
             message: "Please set title for new task list"
         )
         
         alertBuilder
-            .setTextField(withPlaceholder: "List title", andText: taskList?.title)
+            .setTextField(taskList?.title)
             .addAction(title: taskList != nil ? "Update List" : "Save List", style: .default) { [weak self] newValue, _ in
                 if let taskList, let completion {
                     self?.storageManager.edit(taskList, newValue: newValue)
                     completion()
                     return
                 }
+                
                 self?.save(taskList: newValue)
             }
             .addAction(title: "Cancel", style: .destructive)
@@ -117,14 +166,13 @@ private extension TaskListViewController {
         present(alertController, animated: true)
     }
     
-    func save(taskList: String) {
+    private func save(taskList: String) {
         storageManager.save(taskList) { taskList in
             let rowIndex = IndexPath(row: taskLists.index(of: taskList) ?? 0, section: 0)
             tableView.insertRows(at: [rowIndex], with: .automatic)
         }
     }
 }
-
 // MARK: - Setup UI
 private extension TaskListViewController {
     func setupView() {
@@ -132,24 +180,33 @@ private extension TaskListViewController {
         view.backgroundColor = .white
     }
     
+    func setupSubviews(_ subviews: UIView...) {
+        subviews.forEach { subview in
+            view.addSubview(subview)
+        }
+    }
+    
     func setupNavigationBar() {
         title = "Task list"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: .edit,
-            target: self,
-            action: #selector(getter: editButtonItem)
-        )
+        navigationItem.leftBarButtonItem = editButtonItem
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .add,
             target: self,
             action: #selector(addNewTask)
         )
+        
     }
     
     func setConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
     }
 }
 
