e0cfae64af5a67021bb86e21ab058245dc4d826f
CemenVy <semen.vydrin0108@gmail.com>
2023-10-06 14:10:45 +0400

    Work with extension for UITabelViewCell

---
+++ TodoList/Extensions/Extension + UITabelViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  Extension + UITabelViewCell.swift
+//  TodoList
+//
+//  Created by Семен Выдрин on 06.10.2023.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    func configure(with taskList: TaskList) {
+        let currentTasks = taskList.tasks.filter("isComplete = false")
+        var content = defaultContentConfiguration()
+        
+        content.text = taskList.title
+        
+        if taskList.tasks.isEmpty {
+            content.secondaryText = "0"
+            accessoryType = .none
+        } else if currentTasks.isEmpty {
+            content.secondaryText = nil
+            accessoryType = .checkmark
+        } else {
+            content.secondaryText = currentTasks.count.formatted()
+            accessoryType = .none
+        }
+
+        contentConfiguration = content
+    }
+}
